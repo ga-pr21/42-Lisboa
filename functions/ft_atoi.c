c38d1f46a80812191305c8f773717afaba69d4de
gabriel <ga_pr21@hotmail.com>
2023-04-01 01:01:48 +0100

    teste

---
+++ functions/ft_atoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int ft_atoi(char *str);
+
+int ft_atoi(char *str)
+{
+    int i;
+    int sig;
+    int dest;
+
+    i = 0;
+    sig = 0;
+    dest = 0;
+    while(str[i] == '-')
+    {
+        sig++;
+        i++;
+    }
+    while(str[i] >= '0' && str[i] <= '9')
+    {
+        dest = dest * 10 + (str[i] - '0');
+        i++;
+    }
+    if(sig % 2 != 0)
+    {
+        return dest *= -1;
+    }
+    return dest;
+}
