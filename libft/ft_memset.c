fa2c218387f20e13bace6fd4410bc6d84b5f253e
gabriel <ga_pr21@hotmail.com>
2023-04-06 21:18:31 +0100

    atualizaçao de novas funçoes

---
+++ libft/ft_memset.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void *ft_memset(void *ptr, int value, size_t len)
+{
+    unsigned char *p;
+    unsigned char v;
+    int i;
+
+    p = ptr;
+    v = value;
+    while(len-- > 0)
+    {
+      *p++ = v;
+    }
+    return (ptr);
+}
